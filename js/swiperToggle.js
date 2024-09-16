aa407a799f5b7093f5a103d33c52b27465a22c1c
spdlas <89684642129@mail.ru>
2024-09-16 15:18:12 +0300

    second slider

--- js/swiperToggle.js
+++ js/swiperToggle.js
@@ -10,3 +10,18 @@ openSlider.addEventListener("click", function () {
     ? "Скрыть"
     : "Показать все";
 });
+
+let openSliderTech = document.getElementById("openSliderTech");
+let swiperTech = document.querySelector(".swiper-tech");
+let imageRotateTech = document.getElementById("readTextImageTech");
+let textSliderTech = document.getElementById("readTextTitleTech");
+
+openSliderTech.addEventListener("click", function () {
+  swiperTech.classList.toggle("swiper-tech--open");
+  imageRotateTech.classList.toggle("read-text--rotate");
+  textSliderTech.textContent = swiperTech.classList.contains(
+    ".swiper-tech--open"
+  )
+    ? "Скрыть"
+    : "Показать все";
+});
