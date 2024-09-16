662d1ce3152180f55f4ac64701995af950d27441
spdlas <89684642129@mail.ru>
2024-09-16 12:11:26 +0300

    js update

--- js/swiperToggle.js
+++ js/swiperToggle.js
@@ -1,16 +1,12 @@
 let openSlider = document.getElementById("openSlider");
-let swiper = document.querySelector(".swiper");
-let closeSlider = document.querySelector("swiper--open");
+let swiper = document.querySelector(".swiper-partners");
 let textSlider = document.querySelector(".read-text__title");
 let imageRotate = document.querySelector(".read-text__image");
+
 openSlider.addEventListener("click", function () {
   swiper.classList.toggle("swiper--open");
-
-  if (swiper.classList.contains("swiper--open")) {
-    textSlider.textContent = "Скрыть";
-    imageRotate.classList.add("read-text--rotate");
-  } else {
-    imageRotate.classList.remove("read-text--rotate");
-    textSlider.textContent = "Показать все";
-  }
+  imageRotate.classList.toggle("read-text--rotate");
+  textSlider.textContent = swiper.classList.contains("swiper--open")
+    ? "Скрыть"
+    : "Показать все";
 });
