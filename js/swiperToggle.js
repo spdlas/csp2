b4c7f3f6534ed75c9d9833099a96aeda70fa387d
spdlas <89684642129@mail.ru>
2024-09-20 15:23:34 +0300

    slider
    -Сделал по всплытию

--- js/swiperToggle.js
+++ js/swiperToggle.js
@@ -1,27 +1,26 @@
-let openSlider = document.getElementById("openSlider");
 let swiper = document.querySelector(".swiper-partners");
 let textSlider = document.querySelector(".read-text__title");
 let imageRotate = document.querySelector(".read-text__image");
-
-openSlider.addEventListener("click", function () {
-  swiper.classList.toggle("swiper--open");
-  imageRotate.classList.toggle("read-text--rotate");
-  textSlider.textContent = swiper.classList.contains("swiper--open")
-    ? "Скрыть"
-    : "Показать все";
-});
-
-let openSliderTech = document.getElementById("openSliderTech");
-let swiperTech = document.querySelector(".swiper-tech");
 let imageRotateTech = document.getElementById("readTextImageTech");
+let wind = document.querySelector(".main");
+let swiperTech = document.querySelector(".swiper-tech");
 let textSliderTech = document.getElementById("readTextTitleTech");
 
-openSliderTech.addEventListener("click", function () {
-  swiperTech.classList.toggle("swiper-tech--open");
-  imageRotateTech.classList.toggle("read-text--rotate");
-  textSliderTech.textContent = swiperTech.classList.contains(
-    ".swiper-tech--open"
-  )
-    ? "Скрыть"
-    : "Показать все";
+wind.addEventListener("click", function (evt) {
+  let mis = evt.target;
+  if (mis.id == "readTextTitleTech") {
+    swiperTech.classList.toggle("swiper-tech--open");
+    imageRotateTech.classList.toggle("read-text--rotate");
+    textSliderTech.textContent = swiperTech.classList.contains(
+      "swiper-tech--open"
+    )
+      ? "Скрыть"
+      : "Показать все";
+  } else if (mis.id == "openSlider") {
+    swiper.classList.toggle("swiper--open");
+    imageRotate.classList.toggle("read-text--rotate");
+    textSlider.textContent = swiper.classList.contains("swiper--open")
+      ? "Скрыть"
+      : "Показать все";
+  }
 });
